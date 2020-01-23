8a2c0307371667dd72e4a066593b2c24f49a21b1
haleyhammock <robotics@10.1.16.164>
2020-01-23 10:21:59 -0700

    made practicebot project

---
+++ PracticeBot_2020/src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,21 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+
+public class RobotMap {
+
+    //ID constants
+    public static int leftFrontMotorID = 0;
+    public static int rightFrontMotorID = 0;
+    
+    public static int leftBackFrontMotorID = 0;
+    public static int rightBackFrontMotorID = 0;
+
+    public static int feederID = 0;
+    public static int intakeID = 0;
+
+}
