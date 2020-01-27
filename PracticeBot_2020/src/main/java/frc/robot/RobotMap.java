875a032c7ec1187b929311c42381f2e654ebcc30
WaterfordSchool <lewiswillams@comcast.net>
2020-01-27 16:06:25 -0700

    finalized practice bot code

--- PracticeBot_2020/src/main/java/frc/robot/RobotMap.java
+++ PracticeBot_2020/src/main/java/frc/robot/RobotMap.java
@@ -8,8 +8,8 @@ package frc.robot;
 public class RobotMap {
 
     //Motor ID Constants
-    public static final int LEFT_FRONT_MOTOR_ID = 3;
-    public static final int RIGHT_FRONT_MOTOR_ID = 0;
+    public static final int LEFT_FRONT_MOTOR_ID = 0;
+    public static final int RIGHT_FRONT_MOTOR_ID = 3;
     
     //public static final int LEFT_BACK_MOTOR_ID = 0;
     //public static final int RIGHT_BACK_MOTOR_ID = 0;
@@ -24,7 +24,7 @@ public class RobotMap {
 
     //Joystick Constants
     public static final int JOYSTICK_DRIVER_PORT_ID = 0;
-    public static final int JOYSTICK_OPERATOR_PORT_ID =0;
+    public static final int JOYSTICK_OPERATOR_PORT_ID =1;
     public static final int LEFT_AXIS_ID = 1;
     public static final int RIGHT_AXIS_ID = 3;
     public static final int INDEX_BUTTON_ID = 5;
