ae7ed215c9d267d885d0e4323c74f2ddb61bf26d
haleyhammock <robotics@10.1.16.164>
2020-01-30 12:31:50 -0700

    remove unused imports

--- PracticeBot_2020/src/main/java/frc/robot/Robot.java
+++ PracticeBot_2020/src/main/java/frc/robot/Robot.java
@@ -9,7 +9,6 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
