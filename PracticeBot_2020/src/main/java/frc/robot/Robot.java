362775fb726754d63d3638019613fad2a1168fb9
haleyhammock <robotics@10.1.16.164>
2020-02-01 14:43:18 -0700

    updated gyro for auto - heading still won't correct

--- PracticeBot_2020/src/main/java/frc/robot/Robot.java
+++ PracticeBot_2020/src/main/java/frc/robot/Robot.java
@@ -8,12 +8,11 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
-import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -35,31 +34,41 @@ public class Robot extends TimedRobot {
   double rightStickVal; //Assigned in teleopPeriodic
 
   //Sensors
-  private static final SPI.Port kGyroPort = SPI.Port.kOnboardCS0;
-  ADXRS450_Gyro gyro = new ADXRS450_Gyro(kGyroPort); //check SPI, default is CS0, SPI.Port.kMXP
+  Timer counter = new Timer();
+  
+  ADXRS450_Gyro gyro = new ADXRS450_Gyro(); //check SPI, default is CS0, SPI.Port.kMXP
+  //private static final double kVoltsPerDegreePerSecond = 0.0120;
+  //private static final SPI.Port kGyroPort = SPI.Port.kOnboardCS0;
+  private static final double kAngleSetpoint = 0.0;
+  private static final double kP = 0.005; // propotional turning constant
 
   @Override
   public void robotInit() {
-
-    gyro.calibrate(); //sets gyro's current postition to 0
-
+    gyro.calibrate();  //calibrates gyro on robot start
+    gyro.reset();  //sets gyro's current postition to 0
   }
 
   @Override
   public void robotPeriodic() {
+    //autoRoutine();
+    //gyroValue+= Math.ceil(gyro.getAngle()*100-2)/100;
+    //gyroRateRate= Math.abs(gyroRate - gyro.getRate());        
+    //SmartDashboard.putNumber("heading", gyro.getAngle());
+    //SmartDashboard.putNumber("gyro rate change", gyroRateRate);
+    //gyroRate = gyro.getRate();
   }
 
   @Override
   public void autonomousInit() {
-    
-    //gyro.calibrate();
+    counter.reset();
+    counter.start();
+    gyro.reset();
 
   }
 
   @Override
   public void autonomousPeriodic() {
     autoRoutine();
-    //SmartDashboard.putNumber("Gyro Value: ", gyro.getAngle());
 
   }
 
@@ -90,43 +99,75 @@ public class Robot extends TimedRobot {
     leftStickVal = driver.getRawAxis(RobotMap.LEFT_AXIS_ID);
     rightStickVal = driver.getRawAxis(RobotMap.RIGHT_AXIS_ID);
 
-    /*Sets Gyro Turning Value 
-    *double turnVal = (RobotMap.GYRO_SETPOINT - gyro.getAngle()) * RobotMap.GYRO_TURNING_CONSTANT;
-    *turnVal = Math.copySign(turnVal, leftStickVal);
-    *turnVal = Math.copySign(turnVal, rightStickVal);
-    */
-
     //Tank drive method call
     dT.tankDrive(-leftStickVal * RobotMap.DRIVE_SPEED_ID, -rightStickVal * RobotMap.DRIVE_SPEED_ID);
     //dT.arcadeDrive(driver.getY(), turningVal);
 
+    //index and flyWheel methods call
     index();
     flyWheel();
-
-    //SmartDashboard code
-    //SmartDashboard.putNumber("Gyro Value: ", gyro.getAngle());
   }
 
   //Test AutoRoutine
   public void autoRoutine() {
-    //resets gyro to 0
-    //double turningVal = (RobotMap.GYRO_SETPOINT - gyro.getAngle()) * RobotMap.GYRO_TURNING_CONSTANT;
-   // double startAngle = gyro.getAngle(); //should be 0 intially
-
-    //turn right 45 degrees
-    //left off here decreasing turning value as the gyro gets closer to 45 degrees
-    /*if (gyro.getAngle() < 45){
-      //right.set(Math.pow(45 - startAngle / -45, 3));
-      //left.set(Math.pow(45 - startAngle / 45, 3));
-      right.set(-0.3);
-      left.set(0.3);
-    }
-    else{
+    counter.reset();
+    while(counter.get()<3.0){
+      indexMotor.set(RobotMap.INDEX_SPEED_ID);
+      flywheelMotor.set(RobotMap.FLYWHEEL_SLOW_SPEED_ID);
       right.set(0.0);
       left.set(0.0);
     }
-    */
+
+    counter.reset();
+    while(counter.get()<4.0){
+      indexMotor.set(0.0);
+      flywheelMotor.set(0.0);
+      //turn right 45 degrees
+      if (gyro.getAngle() < 90){
+        //right.set(Math.pow(45 - startAngle / -45, 3));
+        //left.set(Math.pow(45 - startAngle / 45, 3));
+        right.set(0.3);
+        left.set(0.3);
+      }
+      //stop drive motors
+      else{
+        right.set(0.0);
+        left.set(0.0);
+      }
+    }
+
+    gyro.reset();
+    counter.reset();
+    while (counter.get()<20){
+      double turningValue = (kAngleSetpoint - gyro.getAngle()) * kP;
+      // Invert the direction of the turn if we are going backwards
+      turningValue = Math.copySign(turningValue, 1);
+      dT.arcadeDrive(0.4, turningValue);
+      SmartDashboard.putNumber("heading", gyro.getAngle());
+      SmartDashboard.putNumber("turning value", -turningValue);
+    }
+    
+    counter.reset();
+    while (counter.get()<1.0){
+      indexMotor.set(0.0);
+      flywheelMotor.set(0.0);
+      right.set(0.0);
+      left.set(0.0);
+    }
+    
+    
+    //left off here decreasing turning value as the gyro gets closer to 45 degrees
+    //double turningVal = (RobotMap.GYRO_SETPOINT - gyro.getAngle()) * RobotMap.GYRO_TURNING_CONSTANT;
+    //double startAngle = gyro.getAngle(); //should be 0 intially
+    
+    //Print values to SmartDashboard
     SmartDashboard.putNumber("Gyro Value: ", gyro.getAngle());
+    SmartDashboard.putNumber("Counter: ", counter.get());
+    //SmartDashboard.putNumber("gyro rate", gyro.getRate());
+
+    //Print values to RioLog
+    //System.out.println(gyro.getAngle());
+    //System.out.println("rate"+gyro.getRate());
   }
 
   @Override
