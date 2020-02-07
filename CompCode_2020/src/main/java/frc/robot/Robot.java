b7e90edfb21569575fb5dec4fd7b79bc0f376886
haleyhammock <robotics@10.1.16.164>
2020-02-07 10:26:26 -0700

    added drive code need to finish robotmap values

--- CompCode_2020/src/main/java/frc/robot/Robot.java
+++ CompCode_2020/src/main/java/frc/robot/Robot.java
@@ -7,13 +7,38 @@
 
 package frc.robot;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 
 
 public class Robot extends TimedRobot {
   
-  //Motors
-  
+  //Left Drive Motors
+  CANSparkMax l1 = new CANSparkMax(20, MotorType.kBrushless);
+  CANSparkMax l2 = new CANSparkMax(21, MotorType.kBrushless);
+  CANSparkMax l3 = new CANSparkMax(25, MotorType.kBrushless);
+
+  //Right Drive Motors
+  CANSparkMax r1 = new CANSparkMax(22, MotorType.kBrushless);
+  CANSparkMax r2 = new CANSparkMax(23, MotorType.kBrushless);
+  CANSparkMax r3 = new CANSparkMax(24, MotorType.kBrushless);
+
+  //Speed Controller Groups
+  SpeedControllerGroup r = new SpeedControllerGroup(r1, r2, r3);
+  SpeedControllerGroup l = new SpeedControllerGroup(l1, l2, l3);
+
+  //Drive Train
+  DifferentialDrive dT = new DifferentialDrive(l, r);
+
+  //Joysticks
+  Joystick driver = new Joystick(0);
+
 
   @Override
   public void robotInit() {
@@ -33,6 +58,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
+    dT.tankDrive(driver.getRawAxis(1) * RobotMap.DRIVE_SPEED, driver.getRawAxis(3) * RobotMap.DRIVE_SPEED);
   }
 
   @Override
---
+++ CompCode_2020/src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,23 @@
+/*----------------------------------------------------------------------------*/
+/* Class contains all constants used in order to maintain cleanliness of code */
+/* In order to call a value use "RobotMap.NAME_OF_VALUE" without quotes       */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+public class RobotMap {
+
+    //Motor ID Constants
+    //Right
+    public static final int R1 = 20;
+    public static final int R2 = 20;
+    public static final int R3 = 20;
+
+    //Left
+    public static final int L1 = 20;
+    public static final int L2 = 20;
+    public static final int L3 = 20;
+
+    //Misc Variables
+    public static final double DRIVE_SPEED = 0.8;
+}
