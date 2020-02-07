3a1d3ec8596d2e05a3bd7f3e6254a6ec129396f6
haleyhammock <robotics@10.1.16.164>
2020-02-07 16:11:34 -0700

    updated robot map for driving and feeder

--- CompCode_2020/src/main/java/frc/robot/RobotMap.java
+++ CompCode_2020/src/main/java/frc/robot/RobotMap.java
@@ -9,15 +9,25 @@ public class RobotMap {
 
     //Motor ID Constants
     //Right
-    public static final int R1 = 20;
-    public static final int R2 = 20;
-    public static final int R3 = 20;
+    public static final int R1 = 22;
+    public static final int R2 = 23;
+    public static final int R3 = 25;
 
     //Left
     public static final int L1 = 20;
-    public static final int L2 = 20;
-    public static final int L3 = 20;
+    public static final int L2 = 21;
+    public static final int L3 = 25;
 
-    //Misc Variables
+    //Feeder Motors
+    public static final int INDEX_ID = 7;
+    public static final int FLYWHEEL_ID = 5;
+
+    //Joystick Variables
+    public static final int JOYSTICK_DRIVER_PORT = 0;
+    public static final int JOYSTICK_OPERATOR_PORT = 1;
+
+    //Speed Variables
     public static final double DRIVE_SPEED = 0.8;
+    public static final double INDEX_SPEED = 0.8;
+    public static final double FLYWHEEL_SPEED = 0.9;
 }
