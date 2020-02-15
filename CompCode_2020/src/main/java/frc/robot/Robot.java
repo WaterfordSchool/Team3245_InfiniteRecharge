26d14f7ee7ab2ea5aa12b372363b29d8f3b8abf9
haleyhammock <robotics@10.1.16.164>
2020-02-14 17:49:42 -0700

    added ramp code

--- CompCode_2020/src/main/java/frc/robot/Robot.java
+++ CompCode_2020/src/main/java/frc/robot/Robot.java
@@ -4,7 +4,7 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
- package frc.robot;
+package frc.robot;
 
 import edu.wpi.first.wpilibj.Timer;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
