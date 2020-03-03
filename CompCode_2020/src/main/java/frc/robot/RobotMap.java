781e19021a20b3b8861f3c86b88012b94bdb36b3
WaterfordSchool <lewiswillams@comcast.net>
2020-03-02 18:42:59 -0700

    changes

--- CompCode_2020/src/main/java/frc/robot/RobotMap.java
+++ CompCode_2020/src/main/java/frc/robot/RobotMap.java
@@ -23,14 +23,14 @@ public class RobotMap {
     public static final int FLYWHEEL_MOTOR_ID = 2;
 
     //Floor Loading Motors
-    public static final int ARM_MOTOR_ID = 8;
-    public static final int INTAKE_MOTOR_ID = 9;
+    public static final int ARM_MOTOR_ID = 10;
+    public static final int INTAKE_MOTOR_ID = 5;
     public static final int UPTAKE_MOTOR_ID = 6;
 
     //Climber Motors
-    public static final int CLIMBER_MOTOR_L = 10;
+    public static final int CLIMBER_MOTOR_L = 8;
     public static final int CLIMBER_MOTOR_R = 4;
-    public static final int CLIMBER_MOTOR_M = 8;
+    public static final int CLIMBER_MOTOR_M = 9;
 
     //Driver Controls
     public static final int DRIVER_PORT = 0;
