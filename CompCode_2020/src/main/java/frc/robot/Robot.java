61d1efb693af347145933357d616f8b81a7da45e
WaterfordSchool <lewiswillams@comcast.net>
2020-03-04 15:24:49 -0700

    removed limit switch code

--- CompCode_2020/src/main/java/frc/robot/Robot.java
+++ CompCode_2020/src/main/java/frc/robot/Robot.java
@@ -71,10 +71,11 @@ public class Robot extends TimedRobot {
   PIDController PID = new PIDController(p, i, d);
 
   //Limit Switch
-  DigitalInput armDownSwitch = new DigitalInput(RobotMap.LIMIT_SWITCH_D_PORT);
+  /**DigitalInput armDownSwitch = new DigitalInput(RobotMap.LIMIT_SWITCH_D_PORT);
   DigitalInput armUpSwitch = new DigitalInput(RobotMap.LIMIT_SWITCH_U_PORT);
   boolean isDown = false;
   boolean isPressed = false;
+  */
 
   @Override
   public void robotInit() {
