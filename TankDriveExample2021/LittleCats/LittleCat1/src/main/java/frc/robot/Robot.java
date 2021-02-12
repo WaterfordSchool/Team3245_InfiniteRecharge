2781283983695c87e4e644fe603fe7113ec956a2
WaterfordSchool <lewiswillams@comcast.net>
2021-02-12 16:06:54 -0700

    created limelight test project

---
+++ TankDriveExample2021/LittleCats/LittleCat1/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,67 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  /**
+   * This function is run when the robot is first started up and should be used
+   * for any initialization code.
+   */
+
+  Talon leftMotor = new Talon(0);
+  Talon rightMotor = new Talon(1);
+  
+  //Controller
+  Joystick logitechControl = new Joystick(0);
+
+  //DifferentialDrive
+  DifferentialDrive driveTrain = new DifferentialDrive(leftMotor, rightMotor);
+
+  @Override
+  public void robotInit() {
+  }
+
+  @Override
+  public void autonomousInit() {
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+  }
+
+  @Override
+  public void teleopInit() {
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    driveTrain.tankDrive(logitechControl.getRawAxis(1), logitechControl.getRawAxis(3));
+  }
+
+  @Override
+  public void testInit() {
+  }
+
+  @Override
+  public void testPeriodic() {
+  }
+
+}
